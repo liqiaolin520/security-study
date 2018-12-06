3176dcb1297a47dac1660213dd435d944943c47f
qiaolin <992004863@qq.com>
2018-12-06 15:28:04 +0800

    6-4--重构用户名密码登陆
    6-4--重构用户名密码登陆
    使其认证成功之后发放Token给请求者

--- security-app/src/main/java/indi/qiaolin/security/app/ResourceServerConfig.java
+++ security-app/src/main/java/indi/qiaolin/security/app/ResourceServerConfig.java
@@ -1,7 +1,19 @@
 package indi.qiaolin.security.app;
 
+import indi.qiaolin.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import indi.qiaolin.security.core.property.SecurityConstants;
+import indi.qiaolin.security.core.property.SecurityProperties;
+import indi.qiaolin.security.core.social.MySpringSocialConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 资源服务器配置
@@ -11,5 +23,60 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @Configuration
 @EnableResourceServer // 必须加上这个注解，否则无法使用令牌访问接口
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler authenticationFailureHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private MySpringSocialConfigurer mySpringSocialConfigurer;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+            .loginPage(SecurityConstants.DEFAULT_UN_AUTHENTICATION_URL)
+            .loginProcessingUrl(SecurityConstants.DEFAULT_AUTHENTICATION_URL_FORM)
+            .successHandler(authenticationSuccessHandler)
+            .failureHandler(authenticationFailureHandler)
+
+            .and()
+            .apply(smsCodeAuthenticationSecurityConfig)
+            .and()
+            .apply(mySpringSocialConfigurer)
+            .and()
+            .authorizeRequests()
+            .antMatchers(buildPermitUrl())
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .csrf()
+            .disable();
+
+    }
+
+    private String[] buildPermitUrl(){
+        List<String> permitUrl = new ArrayList<>();
+
+        permitUrl.add(securityProperties.getBrowser().getLoginPage());
+        permitUrl.add(SecurityConstants.DEFAULT_UN_AUTHENTICATION_URL);
+        permitUrl.add(SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*");
+        permitUrl.add(SecurityConstants.DEFAULT_AUTHENTICATION_URL_MOBILE);
+        permitUrl.add(securityProperties.getBrowser().getSignUpUrl());
+        permitUrl.add("/user/register");
+        permitUrl.add("/social/user");
+        permitUrl.add(securityProperties.getBrowser().getSession().getSessionInvalidUrl() + ".html");
+        permitUrl.add(securityProperties.getBrowser().getSignOutUrl());
+        return  permitUrl.toArray(new String[permitUrl.size()]);
+    }
+
 }
--- security-app/src/main/java/indi/qiaolin/security/app/authentication/MyAuthenticationSuccessHandler.java
+++ security-app/src/main/java/indi/qiaolin/security/app/authentication/MyAuthenticationSuccessHandler.java
@@ -1,11 +1,17 @@
 package indi.qiaolin.security.app.authentication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import indi.qiaolin.security.core.property.LoginType;
 import indi.qiaolin.security.core.property.SecurityProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -34,6 +40,15 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
     @Autowired
     private SecurityProperties securityProperties;
 
+    /** 用于检查第三方应用的clientId和clientSecret是否正确 */
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    /** 生成授权码的服务 */
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+
     /**
      *  认证成功时调用
      * @param request
@@ -46,15 +61,65 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
         log.info("登陆成功！");
+        String header = request.getHeader("Authorization");
 
-        if(LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())){
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().print(objectMapper.writeValueAsString(authentication));
-        }else{
-            super.onAuthenticationSuccess(request, response, authentication);
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无Client信息！");
         }
+
+
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        /** 这个为第三方程序的信息 */
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        if(clientDetails == null){
+            throw new UnapprovedClientAuthenticationException("ClientId对应的信息不存在！");
+        }else if(!clientDetails.getClientSecret().equals(clientSecret)){
+            throw new UnapprovedClientAuthenticationException("ClientSecret不匹配！");
+        }
+
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+
+        OAuth2Request storedRequest = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(storedRequest, authentication);
+
+        // 这个就是生成出来的令牌
+        OAuth2AccessToken accessToken = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().print(objectMapper.writeValueAsString(accessToken));
+
 
     }
 
+    /**
+     * 提取Basic认证信息并解码
+     * @throws BadCredentialsException 如果认证的凭证错误或者是没有的
+     * Base64
+     */
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)throws IOException {
 
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        }catch (IllegalArgumentException e) {
+            throw new BadCredentialsException("解码Basic认证凭证失败");
+        }
+
+        String token = new String(decoded, "UTF-8");
+
+        int delimit = token.indexOf(":");
+
+        if (delimit == -1) {
+            throw new BadCredentialsException("无效的Basic认证凭证！");
+        }
+        return new String[] { token.substring(0, delimit), token.substring(delimit + 1) };
+    }
 }
